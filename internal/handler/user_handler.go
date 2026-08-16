34e8f101985b14d6914c07fa825e537bb2173225
Pidd-pipi <Pidd-pipi@users.noreply.github.com>
2026-08-16 14:27:54 +0800

    gold: user-nil-deposit

--- internal/handler/user_handler.go
+++ internal/handler/user_handler.go
@@ -61,12 +61,12 @@ func (h *UserHandler) GetProfile(c *gin.Context) {
 		util.Fail(c, http.StatusUnauthorized, constants.CodeUnauthorized, constants.MsgUnauthorized)
 		return
 	}
-	_, err = h.svc.GetProfile(c.Request.Context(), userID)
+	user, err := h.svc.GetProfile(c.Request.Context(), userID)
 	if err != nil {
 		c.Error(err)
 		return
 	}
-	util.OK(c, nil)
+	util.OK(c, service.ToUserView(user))
 }
 
 // UpdateProfile handles PUT /users/me.
